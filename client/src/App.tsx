6b81fe59129998da1a8459a03ad616b3d8f6fe89
Aaron Donaldson <atreids@outlook.com>
2022-06-14 15:11:51 +0100

    start of tapps-aff project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,23 @@
-import React from "react";
-import logo from "./logo.svg";
-import weatherDisplay from "./weatherComp";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello</h1>
-        <>{weatherDisplay()}</>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
